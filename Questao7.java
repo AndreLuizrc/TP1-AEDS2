151abcd46da0fb3d36cbd4e0a199f3dfb07cf287
Andre Luiz <andreluizrc27@gmail.com>
2024-03-04 23:49:50 -0300

    finish questão7

--- Questao7.java
+++ Questao7.java
@@ -11,6 +11,7 @@ public class Questao7 {
     
     public static Scanner sc = new Scanner(System.in);
 
+    //Função para ler a URL, iniciar a conexão com o site e retornar o conteudo HTML do mesmo
     public static String getUrl(String endereco) {
         URI uri;
         URL url;
@@ -18,15 +19,15 @@ public class Questao7 {
         BufferedReader br;
         String text = " ", line;
         try{
-            uri = new URI(endereco);
-            url = uri.toURL();
-            is = url.openStream();
-            br = new BufferedReader(new InputStreamReader(is));
+            uri = new URI(endereco); //inicialização da URI passando o endereço como paramentro
+            url = uri.toURL(); // trasnformação da URI para URL
+            is = url.openStream(); //Abertura de conesão com o site
+            br = new BufferedReader(new InputStreamReader(is)); // Buffer para ler o conteudo do site
 
             while((line = br.readLine()) != null){
                 text += line;
             }
-        } catch(MalformedURLException e){
+        } catch(MalformedURLException e){ //catchs para capturar as excessões do código de comunicação
             e.printStackTrace();
         } catch(IOException ioe){
             ioe.printStackTrace();
@@ -34,73 +35,120 @@ public class Questao7 {
             e.printStackTrace();
         }
 
-        //System.out.println(url);
-
         return text;
  
     }
 
     //função para contar vogais presentes no texto da url passada
     public static int[] contVogais(String text){
-        int[] vetor_cont = new int[21];
+        int[] vetor_cont = new int[22];
 
         int tam_string = text.length();
 
         for(int i = 0; i < tam_string; i++){
-            if( text.charAt(i) == 'a' || text.charAt(i) == 'A'){
+            if( text.charAt(i) == 'a' ){
                 vetor_cont[0] += 1;
-            } else if( text.charAt(i) == 'e' || text.charAt(i) == 'E') {
+            } else if( text.charAt(i) == 'e'){
                 vetor_cont[1] += 1;
-            } else if( text.charAt(i) == 'i' || text.charAt(i) == 'I'){
+            } else if( text.charAt(i) == 'i'){
                 vetor_cont[2] += 1;
-            } else if(text.charAt(i) == 'o' || text.charAt(i) == 'O'){
+            } else if(text.charAt(i) == 'o' ){
                 vetor_cont[3] += 1;
-            } else if(text.charAt(i) == 'u' || text.charAt(i) == 'U'){
+            } else if(text.charAt(i) == 'u'){
                 vetor_cont[4] += 1;
-            } else if(text.charAt(i) == 'á' || text.charAt(i) == 'Á'){
+            } else if(text.charAt(i) == 'á'){
                 vetor_cont[5] += 1;
-            } else if(text.charAt(i) == 'é' || text.charAt(i) == 'É'){
+            } else if(text.charAt(i) == 'é'){
                 vetor_cont[6] += 1;
-            } else if(text.charAt(i) == 'í' || text.charAt(i) == 'Í'){
+            } else if(text.charAt(i) == 'í'){
                 vetor_cont[7] += 1;
-            } else if(text.charAt(i) == 'ó' || text.charAt(i) == 'Ó'){
+            } else if(text.charAt(i) == 'ó'){
                 vetor_cont[8] += 1;
-            } else if(text.charAt(i) == 'ú' || text.charAt(i) == 'Ú'){
+            } else if(text.charAt(i) == 'ú'){
                 vetor_cont[9] += 1;
-            } else if(text.charAt(i) == 'à' || text.charAt(i) == 'À'){
+            } else if(text.charAt(i) == 'à'){
                 vetor_cont[10] += 1;
-            } else if(text.charAt(i) == 'è' || text.charAt(i) == 'È'){
+            } else if(text.charAt(i) == 'è'){
                 vetor_cont[11] += 1;
-            } else if(text.charAt(i) == 'ì' || text.charAt(i) == 'Ì'){
+            } else if(text.charAt(i) == 'ì'){
                 vetor_cont[12] += 1;
-            } else if(text.charAt(i) == 'ò' || text.charAt(i) == 'Ò'){
+            } else if(text.charAt(i) == 'ò'){
                 vetor_cont[13] += 1;
-            } else if(text.charAt(i) == 'ù' || text.charAt(i) == 'Ù'){
+            } else if(text.charAt(i) == 'ù'){
                 vetor_cont[14] += 1;
-            } else if(text.charAt(i) == 'ã' || text.charAt(i) == 'Ã'){
+            } else if(text.charAt(i) == 'ã'){
                 vetor_cont[15] += 1;
-            } else if(text.charAt(i) == 'õ' || text.charAt(i) == 'Õ'){
+            } else if(text.charAt(i) == 'õ'){
                 vetor_cont[16] += 1;
-            } else if(text.charAt(i) == 'â' || text.charAt(i) == 'Â'){
+            } else if(text.charAt(i) == 'â'){
                 vetor_cont[17] += 1;
-            } else if(text.charAt(i) == 'ê' || text.charAt(i) == 'Ê'){
+            } else if(text.charAt(i) == 'ê'){
                 vetor_cont[18] += 1;
-            } else if(text.charAt(i) == 'î' || text.charAt(i) == 'Î'){
+            } else if(text.charAt(i) == 'î'){
                 vetor_cont[19] += 1;
-            } else if(text.charAt(i) == 'ô' || text.charAt(i) == 'Ô'){
+            } else if(text.charAt(i) == 'ô'){
                 vetor_cont[20] += 1;
-            } else if(text.charAt(i) == 'û' || text.charAt(i) == 'Û'){
+            } else if(text.charAt(i) == 'û'){
                 vetor_cont[21] += 1;
             }
         }
 
         return vetor_cont;
     }
+
+    //Função para contar consoantes no HTML da URL
+    public static int contConsoantes(String text){
+        int cont_consoantes = 0;
+        int tam_string = text.length();
+        int valorAISCC;
+
+        for(int i = 0; i < tam_string; i++) {
+            valorAISCC = (int) text.charAt(i);
+            if((valorAISCC >= 97 && valorAISCC <= 122)){ //Pego o valor AISCC do caracter para analisar se ele é uma letra OBS: A saida do vdd esta considerando apenas a letras minusculas por isso tirei a verificação das maisculas
+                if (text.charAt(i) != 'a' && text.charAt(i) != 'e' && text.charAt(i) != 'i' && text.charAt(i) != 'o' && text.charAt(i) != 'u' // verifico se ele é um consoante ou uma vogal
+                && text.charAt(i) != 'A' && text.charAt(i) != 'E' && text.charAt(i) != 'I' && text.charAt(i) != 'O' && text.charAt(i) != 'U'){
+                   cont_consoantes += 1;
+               } 
+            }
+        }
+
+        return cont_consoantes;
+    }
+
+    //Função para contar tags <br> no HTML da URL
+    public static int contBr(String text){
+        int tam_string = text.length();
+        int cont_br = 0;
+
+        for(int i = 0; i < tam_string; i++){
+            if (text.charAt(i) == '<' && text.charAt(i+1) == 'b' && text.charAt(i+2) == 'r' && text.charAt(i+3) == '>' ) {
+                cont_br += 1;
+            }
+        }
+
+        return cont_br;
+    }
+
+    //Funão para contar tags <table> no HTML da URL
+    public static int contTable(String text){
+        int tam_string = text.length();
+        int cont_Table = 0;
+
+        for(int i = 0; i < tam_string; i++){
+            if (text.charAt(i) == '<' && text.charAt(i+1) == 't' && text.charAt(i+2) == 'a' && text.charAt(i+3) == 'b' 
+            && text.charAt(i+4) == 'l' && text.charAt(i+5) == 'e' && text.charAt(i+6) == '>' ) {
+                cont_Table += 1;
+            }
+        }
+
+        return cont_Table;
+    }
     public static void main(String[] args) {
         String name, url; 
         String text;
-        int valid = 0;
-        int[] vogais = new int[21];
+        int valid = 0, consoantes, br_tags, table_tags;
+        int[] vogais = new int[22];
+        String result;
         do{
             name = sc.nextLine();
             url = sc.nextLine();
@@ -108,8 +156,15 @@ public class Questao7 {
                 valid = 1;
             }else{
                 text = getUrl(url);
-                vogais = cont_vogais(text);
-                //System.out.println(text);
+                vogais = contVogais(text);
+                consoantes = contConsoantes(text);
+                br_tags = contBr(text);
+                table_tags = contTable(text);
+                result = "a(" + vogais[0] + ") e(" + vogais[1] + ") i(" + vogais[2] + ") o(" + vogais[3] + ") u(" + vogais[4] + ") á(" + vogais[5] + ") é(" + vogais[6] + ") í(" + vogais[7] +
+                ") ó(" + vogais[8] + ") ú(" + vogais[9] + ") à(" + vogais[10] + ") è(" + vogais[11] + ") ì(" + vogais[12] + ") ò(" + vogais[13] + ") ù(" + vogais[14] + ") ã(" + vogais[15] + 
+                ") õ(" + vogais[16] + ") â(" + vogais[17] + ") ê(" + vogais[18] + ") î(" + vogais[19] + ") ô(" + vogais[20] + ") û(" + vogais[21] + ") consoantes(" + consoantes + ") <br>("
+                + br_tags + ") <table>(" + table_tags + ") " + name;
+                System.out.println(result);
             }
         }while(valid != 1);
 
