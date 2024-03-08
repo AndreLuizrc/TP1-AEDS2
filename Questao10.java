f10f61c7a405387cce160fa7ef5683829b8cf51a
André <andreluizrc27@gmail.com>
2024-03-08 08:12:29 -0300

    add Questao10

---
+++ Questao10.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Questao10 {
+
+    public static boolean palindromo(String texto){
+        int tam_string = texto.length();
+        return palindromo(texto, 0, tam_string-1);
+    }
+    //função de verificação de Palíndromo (recebe uma string como parametro e compara os elementos nas posições contrarias)
+    public static boolean palindromo(String texto, int i, int j){
+        int tam_string;
+        boolean valid_poli;
+
+        tam_string = texto.length();
+
+        if(i == tam_string-1){
+            valid_poli = true;
+        } else if(texto.charAt(i) == texto.charAt(j)){ //Uso as minhas variaveis como indice para comparar se os vlaores nas posições contrarias são iguais
+            valid_poli = palindromo(texto, i + 1, j - 1);
+        } else {
+            valid_poli = false;
+        }
+        
+        return valid_poli;
+            
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string;
+        int valid = 0;
+        boolean retorno;
+
+        do{
+            string = scanner.nextLine();
+            if(string.charAt(0) == 'F' && string.charAt(1) == 'I' && string.charAt(2) == 'M'){ 
+                valid = 1;
+            }else{
+                retorno = palindromo(string);
+                if (retorno == true){
+                        System.out.println("SIM");
+                }else{
+                        System.out.println("NAO");
+                }
+            }
+        }while(valid != 1);
+
+        scanner.close();
+    }
+
+ 
+}
+
