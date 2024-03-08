2a15a56216cdc9f740c44b64074661fc78ce0f77
Andre Luiz <andreluizrc27@gmail.com>
2024-03-07 23:33:31 -0300

    resolução de erro Questao8

--- Questao8.java
+++ Questao8.java
@@ -14,25 +14,27 @@ public class Questao8 {
 
     public static void PreencArquivo(int n, File arquivo_temp){
         sc.useLocale(Locale.US);
-        BufferedWriter write = null;
+
         float num;
+        RandomAccessFile file = null;
         try{
-            write = new BufferedWriter(new FileWriter(arquivo_temp));
+
+            file = new RandomAccessFile(arquivo_temp, "rw");
 
             for(int i = 0; i < n; i++){
                 num = sc.nextFloat();
-                //System.out.println("Numero: " + num);
-                write.write(Float.toString(num));
-                if (i != n-1){
-                    write.newLine();
-                }
+                file.writeFloat(num);
+
+                /*if (i != n-1){
+                    file.writeFloat(num);
+                }*/
             }
         } catch (IOException err){
             err.printStackTrace();
         } finally {
             try {
-                if (write != null)
-                    write.close();
+                if (file != null)
+                    file.close();
             }
             catch (Exception ex) {
                 ex.printStackTrace();
@@ -42,18 +44,16 @@ public class Questao8 {
 
     public static void LerArquivo(File arquivo_temp, int n) {
         
-        long position = 0;
+        //long position = 0;
         RandomAccessFile file = null;
 
         try{
             file = new RandomAccessFile(arquivo_temp, "r");
 
-            file.seek(file.length()-4);
-            while(file.getFilePointer() >= 0){
-                position = file.getFilePointer();
-                System.out.println(file.getFilePointer());
-                System.out.println(file.readLine());
-                file.seek(position-4);
+            while( n > 0){
+                file.seek(4*(n-1));
+                System.out.println(file.readFloat());
+                n--;
             }
         } catch(IOException e){
             e.printStackTrace();
