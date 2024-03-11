6479b186ea3a6cab107bc17bd87064bbaef65dd2
André <andreluizrc27@gmail.com>
2024-03-11 20:34:37 -0300

    Modif. Questão 5

--- Questao5.java
+++ Questao5.java
@@ -3,20 +3,39 @@ import java.util.Scanner;
 //2 0 0 and(not(A) , not(B))
 public class Questao5 {
 
-    public boolean AlgebraBoolean(String expressao){
+    public static boolean AlgebraBoolean(String expressao){
         int tam_string = expressao.length();
-        int num_entradas;
-        num_entradas = (int) expressao.charAt(0);
-        int[] valores = new int[num_entradas];
+        int num_entradas, n=0;
+        String expressoes;
+        boolean result;
+        //num_entradas = (int)expressao.charAt(0);
+        //System.out.println(expressao.charAt(0));
+        /*int[] valores = new int[num_entradas];
 
         for(int i = 0; i <= num_entradas; i++){
             valores[i] = expressao.charAt(i+2);
+            System.out.println(valores[i]);
+        }*/
+        for(int i = tam_string; i >= 0; i--){
+            if(expressao.charAt(i) == '('){
+                for(int j = i; j < tam_string; j++){
+                    if(expressao.charAt(j) == ')'){
+                        expressoes = expressao.substring(i, j);
+                        j = tam_string;
+                    }
+                }
+
+                if( expressao.charAt(i-1) == 't'){
+                    result = not(expressoes);
+                } else if(expressao.charAt(i-1) == 'd'){
+                    result = and(expressoes)
+                } else if(expressao.charAt(i-1) == 'r'){
+                    result = or(expressoes)
+                }
+            }
         }
 
-        for(int i = 0; i < tam_string; i++) {
-            
-        }
-
+        return true;
     }
     
     public static void main(String[] args) {
@@ -30,7 +49,7 @@ public class Questao5 {
             if(expressao.charAt(0) == 'F' && expressao.charAt(1) == 'I' && expressao.charAt(2) == 'M'){ 
                 valid = 1;
             }else{
-                retorno = true;
+                retorno = AlgebraBoolean(expressao);
                 if (retorno  == true){
                     System.out.println("SIM");
                 }else {
